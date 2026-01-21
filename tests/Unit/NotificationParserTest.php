147bdd37e2003ffc39cd4e5b009a9ea0de0d7571
João Pedro de Toledo <jpedrotoledo@usp.br>
2026-01-20 22:21:24 -0300

    ajusta testes do parser de notificação

--- tests/Unit/NotificationParserTest.php
+++ tests/Unit/NotificationParserTest.php
@@ -23,7 +23,7 @@ class NotificationParserTest extends TestCase
     }
 
     #[Test]
-    public function deve_extrair_valor_e_loja_de_compra_simples()
+    public function deve_extrair_valor_e_loja_simples()
     {
         // Cenário: Compra padrão
         $texto = "Compra de R$ 30,00 no Mercado Livre";
@@ -35,6 +35,23 @@ class NotificationParserTest extends TestCase
         $this->assertEquals(30.00, $resultado['valor']); // Valor limpo (float)
         $this->assertEquals('Mercado Livre', $resultado['loja']); // Nome da loja
         $this->assertEquals(1, $resultado['parcelas']); // Padrão é 1x
+    }
+
+    #[Test]
+    public function deve_lidar_com_formato_nubank()
+    {
+        $texto = "Compra de R$ 24,20 APROVADA em OXXO ALCEU DE CAMPOS para o cartão com final 1234";
+
+        $resultado = ParserService::analisar($texto);
+
+        $this->assertEquals('criar_despesa', $resultado['acao']);
+        $this->assertEquals(24.20, $resultado['valor']);
+        
+        // Testando a extração do cartão
+        $this->assertEquals('1234', $resultado['cartao']);
+
+        // A loja continua sendo o desafio final de limpeza
+        $this->assertEquals('OXXO ALCEU DE CAMPOS', $resultado['loja']);
     }
 
     #[Test]
